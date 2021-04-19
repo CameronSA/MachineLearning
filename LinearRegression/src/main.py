46d1d20f097130135d3540e33096a1ab94914904
cameron simpson-allsop <cammeronallsop95@hotmail.co.uk>
2021-04-19 19:50:12 +0100

    added hypothesis and cost function calculations - the cost function calculations don't seem quite right

--- LinearRegression/src/main.py
+++ LinearRegression/src/main.py
@@ -4,4 +4,4 @@ import matplotlib.pyplot as plt
 if __name__ == '__main__':
     data = generate_poly_data([0, 1], 100, 0)
     data.plot(kind='scatter', x='x', y='y', color='blue')
-    plt.show()
+    
